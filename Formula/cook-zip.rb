c9af787e7d60e6f3f8b5be6c61d386c4a9b3473d
wappon28dev <wappon_28_dev@ruru.be>
2024-06-16 16:17:43 +0900

    🐛 cook-zip v2.1.1 のハッシュが違う

--- Formula/cook-zip.rb
+++ Formula/cook-zip.rb
@@ -5,7 +5,7 @@ class CookZip < Formula
 
   if OS.mac?
     url "https://github.com/wappon28dev/prg1k/releases/download/v2.1.1/cook-zip-macos-x64"
-    sha256 "a66154c2c851806055562828adba201eea97b2a81fda3c0d4e991a59a323784c"
+    sha256 "af1f0b05e89edb33659c8f0990a611e23aca15effba51921f4c471b808cd9668"
   elsif OS.linux?
     url "https://github.com/wappon28dev/prg1k/releases/download/v2.1.1/cook-zip-linux-x64"
     sha256 "620bae606295dec0c9e41f6018b7fa026705ec70e018ab5e0b3cdb658806c3b9"
