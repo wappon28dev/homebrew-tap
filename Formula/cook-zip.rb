ee6b577e8937090600e166f9fe2394712b673f2e
wappon28dev <wappon_28_dev@ruru.be>
2024-07-04 09:58:08 +0900

    ⬆️ cook-zip v2.5.0

--- Formula/cook-zip.rb
+++ Formula/cook-zip.rb
@@ -1,14 +1,14 @@
 class CookZip < Formula
   desc "プログラミング実習の提出物を ZIP にするよ"
   homepage "https://github.com/wappon28dev/prg1k"
-  version "2.4.1"
-
-  if OS.mac?
-    url "https://github.com/wappon28dev/prg1k/releases/download/v2.4.1/cook-zip-macos-x64"
-    sha256 "bc827f8af581c216f247ab000178463d6dc637e90dd5407ec3a2296f68009907"
-  elsif OS.linux?
-    url "https://github.com/wappon28dev/prg1k/releases/download/v2.4.1/cook-zip-linux-x64"
-    sha256 "835e840d609041f56e128a98d037fc8bdabfd8d26e2dbafdd722ede9374906d0"
+  version "2.5.0"
+  
+  if OS.linux?
+    url "https://github.com/wappon28dev/prg1k/releases/download/v2.5.0/cook-zip-linux-x64"
+    sha256 "e7b383aeb27515476bb2aa213f9052e7fe08d3b02962128a11e4b7b71ece1cf6"  
+  elsif OS.mac?
+    url "https://github.com/wappon28dev/prg1k/releases/download/v2.5.0/cook-zip-macos-x64"
+    sha256 "806139c1c34346e5efef5dc2ad05e0ba2b424798f26edd71a46ec7df29a14ed3"
   end
 
   license "MIT"
